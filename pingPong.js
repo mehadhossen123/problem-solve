c3ed5f05ffa048a190c84e092ac75cc2e01155be
Mehad Hossen <mehad004938@gmail.com>
2026-01-13 09:48:35 +0600

    Alhamdulliha done problem-10

---
+++ pingPong.js
@@ -0,0 +1,15 @@
+function getPingPong(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("pingPong");
+    } else if (i % 3 === 0) {
+      console.log("ping");
+    } else if (i % 5 === 0) {
+      console.log("pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+const result = getPingPong(20);
+console.log(result);
